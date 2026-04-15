84dc495b4dc0f2c254d6770c9cb7939430556036
Carcellarcpe <qjecarcellar@tip.edu.ph>
2026-04-16 02:55:48 +0800

    Add files via upload

---
+++ screen/Supplier/ProductInventory.py
@@ -0,0 +1,15 @@
+from kivymd.uix.screen import MDScreen
+from kivy.app import App
+from kivy.lang import Builder
+import os
+
+
+Builder.load_file(os.path.join(os.path.dirname(__file__), "productinventory.kv"))
+
+
+class ProductInventoryScreen(MDScreen):
+
+    def go_back(self):
+        app = App.get_running_app()
+        print("SCREENS:", app.home_manager.screen_names)
+        app.home_manager.current = "account_scr"
